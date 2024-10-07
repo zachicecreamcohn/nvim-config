4b5bc297b838013e23bd1d0e02e84fc2be565504
Zach Cohn <zcohn@wustl.edu>
2024-10-07 12:10:09 -0500

    added fzf to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,7 @@ telescope.setup({
 		},
 	},
 })
+
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
@@ -16,8 +17,18 @@ vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help ta
 vim.keymap.set("n", "<leader>fu", function()
 	builtin.find_files({ cwd = "~" })
 end, { desc = "Telescope find files in user dir" })
+
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
+	},
 }
