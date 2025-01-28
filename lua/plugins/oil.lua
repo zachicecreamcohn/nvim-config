fd8cddc3c7d3d95185b4e6af800160cddc44579b
Zach Cohn <zcohn@wustl.edu>
2025-01-28 14:05:38 -0600

    added detail view command for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,4 +8,24 @@ return {
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 	lazy = false,
+
+	-- a function to config and set up
+	config = function()
+		local detail = false
+		require("oil").setup({
+			keymaps = {
+				["gd"] = {
+					desc = "Toggle file detail view",
+					callback = function()
+						detail = not detail
+						if detail then
+							require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+						else
+							require("oil").set_columns({ "icon" })
+						end
+					end,
+				},
+			},
+		})
+	end,
 }
