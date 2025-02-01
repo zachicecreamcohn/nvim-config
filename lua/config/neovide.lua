acd9187582ccd20e110376ac6993d8415b9e2762
Zach Cohn <zcohn@wustl.edu>
2025-02-01 00:38:41 -0600

    transparency and autopair

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,6 +1,6 @@
 -- optionally, use opacity
--- vim.g.neovide_transparency = 0.95
--- vim.g.neovide_normal_opacity = 0.95
+vim.g.neovide_transparency = 0.90
+vim.g.neovide_normal_opacity = 0.90
 vim.g.neovide_cursor_animation_length = 0.02
 
 vim.g.neovide_scale_factor = 0.95
