25f58ba778bfda7c1f9473927db89896dc1cc29b
Zach Cohn <zcohn@wustl.edu>
2025-02-17 22:59:35 -0600

    add keymap for todo telescope finder

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,9 +1,8 @@
 return {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
+	config = function()
+		require("todo-comments").setup()
+		vim.api.nvim_set_keymap("n", "<leader>td", ":TodoTelescope<CR>", { noremap = true, silent = true })
+	end,
 }
